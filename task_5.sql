eb723acc2eb6d3ef20ca8b5c1aa732f0d45a842e
Robertngatia25 <ngatiah.robert@gmail.com>
2025-06-29 11:27:26 +0300

    Add the insert prompt script sql

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,5 @@
 -- task_5.sql
 USE alx_book_store;
 
-
 INSERT INTO Customer (customer_id, customer_name, email, address)
-
-
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+            VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
